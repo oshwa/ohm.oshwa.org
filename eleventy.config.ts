ad0e6db5919cf359f546b4c09c5e8c1118a6e7f2
Thea Flowers <thea@winterbloom.com>
2024-07-30 12:12:47 -0400

    Initial site setup with 11ty

--- eleventy.config.ts
+++ eleventy.config.ts
@@ -1,7 +1,18 @@
-export default function (eleventyConfig) {
+// Note: Eleventy type definitions are not yet available, hence the use of `any` in this file.
+
+export default function (eleventyConfig: any) {
+    eleventyConfig.addPassthroughCopy("src/static");
+
+    eleventyConfig.setTemplateFormats([
+        // Templates:
+        "html",
+        "md",
+    ]);
+
     return {
         dir: {
             input: "src",
+            includes: "_includes",
             output: "build",
         },
     };
---
+++ src/_data/metadata.js
@@ -0,0 +1,10 @@
+export default {
+    "name": "Open Hardware Month",
+    "github_url": "https://github.com/oshwa/ohm.oshwa.org",
+    "copyright": {
+        "author": "Open Source Hardware Association",
+        "year": "2024",
+        "license": "CC BY-SA 4.0",
+        "license_url": "https://github.com/oshwa/ohm.oshwa.org/blob/main/docs/LICENSE"
+    },
+}
